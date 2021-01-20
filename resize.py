f68147716ab14974c83840ed504fb3299a555fd2
mihir3445 <27816594+mihir3445@users.noreply.github.com>
2021-01-20 10:19:49 +0800

    faster resize

--- resize.py
+++ resize.py
@@ -1,27 +1,23 @@
 import time
 
-window.activate('gedit', switchDesktop=True)
-window.resize_move('gedit', xOrigin=0, yOrigin=0, width=200, height=200, matchClass=False)
+window.activate('Development', switchDesktop=True)
+window.resize_move('Development', xOrigin=-25, yOrigin=-25, width=200, height=200, matchClass=False)
 
 keyboard.press_key('<alt>')
 keyboard.press_key('<f8>')
 keyboard.release_key('<alt>')
 keyboard.release_key('<f8>')
 
+keyboard.press_key('<shift>')
+
 keyboard.press_key('<down>')
-time.sleep(4)
+time.sleep(3)
 keyboard.release_key('<down>')
 
 keyboard.press_key('<right>')
-time.sleep(4)
+time.sleep(5)
 keyboard.release_key('<right>')
+keyboard.release_key('<shift>')
 
 # mouse buttons: left=1, middle=2, right=3
-mouse.click_relative_self(5120, 100, 3)
-
-#geo = window.get_active_geometry()
-#    winGeometry = 'X-origin: %s\nY-origin: %s\nWidth: %s\nHeight: %s'  %(geo[0], geo[1], geo[2], geo[3])
-#    dialog.info_dialog(title='Window geometry', message=winGeometry)
-    
-#winClass = window.get_active_class()
-#dialog.info_dialog(title='Window class', message=winClass)
+mouse.click_relative_self(100, 100, 3)
